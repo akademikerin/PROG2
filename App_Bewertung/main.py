d700691decb3a97eef68a18c8f1960f4e7da7155
akademikerin <71708802+akademikerin@users.noreply.github.com>
2020-11-22 17:12:01 +0100

    Dropdown Eingabe und Restaurant html hinzugefügt, diverse Korrekturen

--- App_Bewertung/main.py
+++ App_Bewertung/main.py
@@ -26,10 +26,19 @@ def eingabe():
         bewertung_aussehen_dessert = request.form['sterne_aussehen_dessert']
         bewertung_geschmack_dessert = request.form['sterne_geschmack_dessert']
         bewertung_menge_dessert = request.form['sterne_menge_dessert']
-        bewertungseingabe = {"Aussehen_Vorspeise": bewertung_aussehen_vorspeise, "Geschmack Vorspeise": bewertung_geschmack_vorspeise, "Menge Vorspeise": bewertung_menge_vorspeise, "Aussehen Hauptspeise": bewertung_aussehen_hauptspeise, "Geschmack Hauptspeise": bewertung_geschmack_hauptspeise, "Menge Hauptspeise": bewertung_menge_hauptspeise, "Aussehen Dessert": bewertung_aussehen_dessert,"Geschmack Dessert": bewertung_geschmack_dessert, "Menge Dessert": bewertung_menge_dessert}
+        bewertung_service = request.form['sterne_service']
+        anmerkungen = request.form['anmerkungen']
+        bewertungseingabe = {"Aussehen_Vorspeise": bewertung_aussehen_vorspeise, "Geschmack Vorspeise": bewertung_geschmack_vorspeise, "Menge Vorspeise": bewertung_menge_vorspeise, "Aussehen Hauptspeise": bewertung_aussehen_hauptspeise, "Geschmack Hauptspeise": bewertung_geschmack_hauptspeise, "Menge Hauptspeise": bewertung_menge_hauptspeise, "Aussehen Dessert": bewertung_aussehen_dessert, "Geschmack Dessert": bewertung_geschmack_dessert, "Menge Dessert": bewertung_menge_dessert, "Service": bewertung_service, "Anmerkungen": anmerkungen}
         antwort = speichern(bewertungseingabe)
-        return 'Gespeicherte Daten: <br>' + str(antwort)
-    return render_template('eingabe.html', app_name="Bewertung abgeben")
+        return 'Gespeicherte Daten:' + str(bewertungseingabe) + ' <br>' + str(antwort)
+    return render_template('eingabe.html', app_name="Bewertung abgeben", vorspeisen=['Salat', 'Suppe', 'Tatar'])
+
+
+@app.route('/restaurant')
+def restaurant():
+    ueberschrift_txt = "Willkommen im Restaurant Hirschen"
+    einleitung_txt = "Hier finden Sie unsere aktuelle Speisekarte"
+    return render_template('start.html', app_name="Restaurant", ueberschrift=ueberschrift_txt, einleitung=einleitung_txt)
 
 
 @app.route('/about')
